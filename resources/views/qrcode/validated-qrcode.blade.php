a42037ed940adc09b095991bbd4dd4b4cda0d01a
ruijiangzeng <zengruijiang@gmail.com>
2024-08-04 22:13:38 +1000

    log

--- resources/views/qrcode/validated-qrcode.blade.php
+++ resources/views/qrcode/validated-qrcode.blade.php
@@ -9,7 +9,7 @@
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
             <div class="bg-white overflow-hidden shadow-xl sm:rounded-lg">
                 <div class="p-6 lg:p-8 bg-white border-b border-gray-200">
-                    <x-application-logo class="block h-12 w-auto"/>
+                    <x-diamond-territory-logo class="block h-auto w-auto"/>
 
                     <h1 class="mt-8 text-2xl font-medium text-gray-900">
                         Welcome back {{ $user->name }}!

e36e9d8b808c41f20e1665ce19b0bba08e01f810
ruijiangzeng <zengruijiang@gmail.com>
2024-08-04 18:30:12 +1000

    add company

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,9 +2,11 @@
 
 namespace Database\Seeders;
 
+use App\Models\Company;
 use App\Models\User;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Arr;
 use Illuminate\Support\Facades\Hash;
 
 class DatabaseSeeder extends Seeder
@@ -15,11 +17,44 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        User::factory()->create([
-            'name' => 'Ryan Zeng',
-            'email' => 'zengruijiang@gmail.com',
-            'mobile' => '0438001663',
-            'password' => Hash::make('password')
-        ]);
+        foreach ($this->getData() as $companyData) {
+            $company = Company::create([
+                'name' => $companyData['name'],
+                'zh_name' => $companyData['zh_name'],
+            ]);
+
+            foreach ($companyData['members'] as $member) {
+                $user = User::create([
+                    'name' => Arr::get($member,'name'),
+                    'email' => Arr::get($member,'email'),
+                    'mobile' => Arr::get($member,'mobile'),
+                    'password' => Hash::make('password'),
+                    'company_id' => $company->id,
+                ]);
+            }
+        }
+
+    }
+
+    private function getData(): array
+    {
+        return [
+            [
+                'name' => 'Diamond Territory',
+                'zh_name' => '御宴',
+                'members' => [
+                    [
+                        'name' => 'Ryan Zeng',
+                        'email' => 'zengruijiang@gmail.com',
+                        'mobile' => '0438001663',
+                    ],
+                    [
+                        'name' => 'Andrew',
+                        'email' => 'Andrew@gmail.com',
+                        'mobile' => '0400000000',
+                    ],
+                ]
+            ]
+        ];
     }
 }
